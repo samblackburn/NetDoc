a3ce6272d029790ba05e26dfa635e301f1240c47
Sam Blackburn <sam.blackburn@red-gate.com>
2020-02-21 16:48:36 +0000

    List methods in consumed C#

--- Rewriter.cs
+++ Rewriter.cs
@@ -1,24 +1,115 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace NetDoc
 {
     internal class Rewriter
     {
-        public Rewriter(IEnumerable<Call> calls)
+        private readonly IEnumerable<Call> m_Calls;
+
+        public Rewriter(IEnumerable<Call> mCalls)
         {
-            
+            m_Calls = mCalls;
         }
 
         internal async Task<Document> Rewrite(Document doc)
         {
-            var tree = await doc.GetSyntaxRootAsync();
+            var model = await doc.GetSyntaxRootAsync();
 
-            // For each method or property
+            foreach (var method in model.DescendantNodes())
+            {
+                //Debug(method);
+            }
 
+            foreach (var method in model.DescendantNodes().Where(CanBeCallTarget))
+            {
+                var containingClass = method.Ancestors().First(IsType);
+                var containingNamespace = string.Join(".", method.Ancestors().Where(IsNamespace)
+                    .Select(n => Name(n.ChildNodes().First(t => t.Kind() == SyntaxKind.IdentifierName))));
+                var isPublic = IsPublic(containingClass) && IsPublic(method);
+                if (!isPublic)
+                {
+                    //continue;
+                }
+
+                var name = Name(method);
+                if (method.Kind() == SyntaxKind.FieldDeclaration)
+                {
+                    name = method
+                        .ChildNodes().Single(n => n.Kind() == SyntaxKind.VariableDeclaration)
+                        .ChildNodes().Single(n => n.Kind() == SyntaxKind.VariableDeclarator)
+                        .ChildTokens().Single(n => n.Kind() == SyntaxKind.IdentifierToken).Text;
+                }
+
+                Console.WriteLine($"{method.Kind()} {containingNamespace}.{Name(containingClass)}.{name}()");
+            }
 
             return doc;
+        }
+
+        private static void Debug(SyntaxNode method)
+        {
+            var spaces = new string(' ', method.Ancestors().Count());
+            var tokens = method.ChildTokens().Select(t => t.Kind() + ":" + t.Text);
+            Console.WriteLine($"{spaces}{method.Kind()} => {string.Join(", ", tokens)}");
+        }
+
+        private static bool IsPublic(SyntaxNode containingClass)
+        {
+            return containingClass.ChildTokens().Any(t => t.Kind() == SyntaxKind.PublicKeyword);
+        }
+
+        private static string Name(SyntaxNode node)
+        {
+            return node.ChildTokens().SingleOrDefault(t => t.Kind() == SyntaxKind.IdentifierToken).Text;
+        }
+
+        private static bool IsNamespace(SyntaxNode n)
+        {
+            return n.Kind() == SyntaxKind.NamespaceDeclaration;
+        }
+
+        private static bool IsType(SyntaxNode n)
+        {
+            switch (n.Kind())
+            {
+                case SyntaxKind.ClassDeclaration:
+                case SyntaxKind.StructDeclaration:
+                case SyntaxKind.InterfaceDeclaration:
+                case SyntaxKind.EnumDeclaration:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private static bool CanBeCallTarget(SyntaxNode n)
+        {
+            switch (n.Kind())
+            {
+                case SyntaxKind.DelegateDeclaration:
+                case SyntaxKind.EnumMemberDeclaration:
+                case SyntaxKind.FieldDeclaration:
+                case SyntaxKind.EventFieldDeclaration:
+                case SyntaxKind.MethodDeclaration:
+                case SyntaxKind.OperatorDeclaration:
+                case SyntaxKind.ConversionOperatorDeclaration:
+                case SyntaxKind.ConstructorDeclaration:
+                case SyntaxKind.PropertyDeclaration:
+                case SyntaxKind.EventDeclaration:
+                case SyntaxKind.IndexerDeclaration:
+                case SyntaxKind.GetAccessorDeclaration:
+                case SyntaxKind.SetAccessorDeclaration:
+                case SyntaxKind.AddAccessorDeclaration:
+                case SyntaxKind.RemoveAccessorDeclaration:
+                case SyntaxKind.UnknownAccessorDeclaration:
+                    return true;
+                default: return false;
+            }
         }
     }
 }
