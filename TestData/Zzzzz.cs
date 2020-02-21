951d0edfb5084f5e8ddc94a03b61ba669dd16188
Sam Blackburn <sam.blackburn@red-gate.com>
2020-02-21 17:14:04 +0000

    Move to folder (since namespace has a dot) and make more structured

---
+++ TestData/Zzzzz.cs
@@ -0,0 +1,23 @@
+﻿namespace NetDoc.TestData
+{
+    public class Zzzzz
+    {
+        public int Field;
+        private void CallerOfField() => Field = 1;
+
+        public int Property { get; set; }
+        private int  CallerOfGetter() => Property;
+        private void CallerOfSetter() => Property = 1;
+
+        public int Method() => 1;
+        private void CallerOfMethod() => Method();
+
+        public int this[int i]
+        {
+            get => 1;
+            set { }
+        }
+        private int CallerOfIndexGetter() => this[2];
+        private int CallerOfIndexSetter() => this[2] = 1;
+    }
+}
