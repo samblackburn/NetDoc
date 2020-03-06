703aadb7eb6f7b09284d7376376e6b6eb8e632cd
Sam Blackburn <sam.blackburn@red-gate.com>
2020-03-06 14:03:26 +0000

    Exclude the library itself from the consumer list!

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -11,15 +12,23 @@ namespace NetDoc
         static void Main()
         {
             const string sln = @"C:\Work\SQLCompareEngine\SQLCompare.sln";
-            Console.WriteLine("Analysing Assembly...");
             var nonObfuscatedBuildFolder = @"C:\Work\SQLDependencyTracker\Build\Debug\net472";
-            var assemblies = Directory.EnumerateFiles(nonObfuscatedBuildFolder, "RedGate.*.dll")
-                .Concat(Directory.EnumerateFiles(nonObfuscatedBuildFolder, "RedGate.*.exe"));
+            var assemblies = RedgateAssembliesInFolder(nonObfuscatedBuildFolder, Path.GetDirectoryName(sln)).ToList();
+            Console.WriteLine("Analysing {0} Assemblies", assemblies.Count());
             var calls = assemblies.SelectMany(AssemblyAnalyser.AnalyseAssembly).ToList();
             Console.WriteLine("Modifying solution...");
             var modifier = new SolutionModifier(new [] {new Rewriter(calls)}, sln);
 
             DumpErrors(modifier.ModifySolution);
+        }
+
+        private static IEnumerable<string> RedgateAssembliesInFolder(string include, string exclude)
+        {
+            var allAssemblies = Directory.EnumerateFiles(include, "RedGate.*.dll", SearchOption.AllDirectories)
+                .Concat(Directory.EnumerateFiles(include, "RedGate.*.exe", SearchOption.AllDirectories));
+            var consumedAssemblies = Directory.EnumerateFiles(exclude, "RedGate.*.dll", SearchOption.AllDirectories)
+                .Select(Path.GetFileName).ToHashSet();
+            return allAssemblies.Where(x => !consumedAssemblies.Contains(Path.GetFileName(x)));
         }
 
         private static void DumpErrors(Func<Task> asyncMethod)
