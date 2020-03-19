10541ddd169e2a5f1d2886a12e66f5f616683fb5
Sam Blackburn <sam.blackburn@red-gate.com>
2020-03-19 17:12:14 +0000

    Too slow against Compare

--- Program.cs
+++ Program.cs
@@ -11,8 +11,10 @@ namespace NetDoc
     {
         static void Main()
         {
-            const string sln = @"C:\Work\SQLCompareEngine\SQLCompare.sln";
-            var nonObfuscatedBuildFolder = @"C:\Work\SQLDependencyTracker\Build\Debug\net472";
+            //const string sln = @"C:\Work\SQLCompareEngine\SQLCompare.sln";
+            //var nonObfuscatedBuildFolder = @"C:\Work\SQLDependencyTracker\Build\Debug\net472";
+            const string sln = @"C:\Users\Sam.Blackburn\source\repos\NetDoc\NetDoc.sln";
+            var nonObfuscatedBuildFolder = @"C:\Users\Sam.Blackburn\source\repos\NetDoc\bin\Debug\net472";
             var assemblies = RedgateAssembliesInFolder(nonObfuscatedBuildFolder, Path.GetDirectoryName(sln)).ToList();
             Console.WriteLine("Analysing {0} Assemblies", assemblies.Count());
             var calls = assemblies.SelectMany(AssemblyAnalyser.AnalyseAssembly).ToList();
@@ -25,7 +27,7 @@ namespace NetDoc
         private static IEnumerable<string> RedgateAssembliesInFolder(string include, string exclude)
         {
             var allAssemblies = Directory.EnumerateFiles(include, "RedGate.*.dll", SearchOption.AllDirectories)
-                .Concat(Directory.EnumerateFiles(include, "RedGate.*.exe", SearchOption.AllDirectories));
+                .Concat(Directory.EnumerateFiles(include, "*.exe", SearchOption.AllDirectories));
             var consumedAssemblies = Directory.EnumerateFiles(exclude, "RedGate.*.dll", SearchOption.AllDirectories)
                 .Select(Path.GetFileName).ToHashSet();
             return allAssemblies.Where(x => !consumedAssemblies.Contains(Path.GetFileName(x)));
