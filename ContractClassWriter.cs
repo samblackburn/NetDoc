6d633cc356f380a3eb0f6b60e1d2689f18a04dbd
Sam Blackburn <sam.blackburn@red-gate.com>
2020-03-20 15:50:01 +0000

    WIP

---
+++ ContractClassWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetDoc
+{
+    internal class ContractClassWriter
+    {
+        public void ProcessCalls(string consumerName, IEnumerable<Call> calls)
+        {
+            Console.WriteLine($"public void UsedBy{consumerName}");
+            Console.WriteLine("{");
+
+            foreach (var call in calls)
+            {
+                Console.WriteLine(call);
+            }
+
+            Console.WriteLine("}");
+        }
+    }
+}
