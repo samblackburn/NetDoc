c969bc9554574564fc2965e9223f3206ced86f2d
Sam Blackburn <sam.blackburn@red-gate.com>
2020-03-27 16:18:40 +0000

    Variables for the parameters as well as the instances to call methods on

--- Call.cs
+++ Call.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
@@ -31,15 +32,20 @@ namespace NetDoc
 
         private static string MakeVariableName(TypeReference type)
         {
-            var lowerCase = type.Name.Substring(0, 1).ToLower() +
-                            type.Name.Substring(1);
-            switch (lowerCase)
+            var lowerCase = type.FullName.Substring(0, 1).ToLower() +
+                            type.FullName.Substring(1);
+            var noDots = lowerCase.Replace(".", "_").Replace("!", "_bang_");
+            var noGenerics = noDots.Replace("<", "_lt_").Replace(">", "_gt_");
+            var noSlashes = noGenerics.Replace(" / ", "_");
+            var withoutIndexer = noSlashes.Replace("[", "_array").Replace(",", "_comma_").Replace("]", "");
+            switch (withoutIndexer)
             {
                 case "object":
                 case "string":
-                    return "@" + lowerCase;
+                case "char":
+                    return "@" + withoutIndexer;
                 default:
-                    return lowerCase.Replace("`", "_");
+                    return withoutIndexer.Replace("`", "");
             }
         }
 
@@ -47,13 +53,60 @@ namespace NetDoc
             get
             {
                 var parameters = string.Join(", ", m_Operand.Parameters.Select(p => p.ParameterType).Select(MakeVariableName));
-                return $".{m_Operand.Name}({parameters})";
+                var indexerParameters = string.Join(", ", m_Operand.Parameters.Skip(1).Select(p => p.ParameterType).Select(MakeVariableName));
+
+                if (m_Operand.Name == ".ctor")
+                {
+                    return $"new {TypeWithGenerics}({parameters});";
+                }
+
+                if (m_Operand.Name == "get_Item")
+                {
+                    return AssignToRandomVariable(m_Operand.ReturnType, $"{VariableName}[{indexerParameters}];");
+                }
+
+                if (m_Operand.Name == "set_Item")
+                {
+                    return $"{VariableName}[{indexerParameters}] = {MakeVariableName(m_Operand.Parameters.First().ParameterType)};";
+                }
+
+                if (m_Operand.Name.StartsWith("get_"))
+                {
+                    return AssignToRandomVariable(m_Operand.ReturnType, $"{VariableName}.{Method};");
+                }
+
+                if (m_Operand.Name.StartsWith("set_"))
+                {
+                    return $"{VariableName}.{Method} = {MakeVariableName(m_Operand.Parameters.First().ParameterType)};";
+                }
+
+                return $"{VariableName}.{m_Operand.Name}({parameters});";
             }
+        }
+
+        private string AssignToRandomVariable(TypeReference returnType, string expression)
+        {
+            return $"{GetTypeName(returnType)} v{Math.Abs(expression.GetHashCode() % 10000):D4} = {expression}";
         }
 
         public bool IsStatic => !m_Operand.HasThis;
         public string TypeWithGenerics => GetTypeName(m_Operand.DeclaringType);
 
+        public IEnumerable<string> ParameterTypes
+        {
+            get
+            {
+                var result = m_Operand.Parameters.Select(p =>
+                    $"        {GetTypeName(p.ParameterType)} {MakeVariableName(p.ParameterType)}");
+                if (result.Any(x => x.Contains("&")))
+                {
+
+                }
+                return result;
+            }
+        }
+
+        public string TypeAsParameter => $"        {TypeWithGenerics} {MakeVariableName(m_Operand.DeclaringType)}";
 
         private string IgnorePropertyPrefix(string name)
         {
@@ -67,14 +120,16 @@ namespace NetDoc
 
         public override string ToString() => $"{TypeWithGenerics}{Invocation}";
 
-        private  string GetTypeName(TypeReference type)
+        private static string GetTypeName(TypeReference type)
         {
             var nameSpace = type.Namespace;
             var className = type.Name.Split('`')[0];
+            if (className.StartsWith("!!")) return className.Replace("!!", "T");
             var generics = type is GenericInstanceType git
-                ? $"<{String.Join(", ", git.GenericArguments.Select(x => x.FullName.Replace("!!", "T")))}>"
+                ? $"<{String.Join(", ", git.GenericArguments.Select(x => GetTypeName(x)))}>"
                 : "";
-            return $"{nameSpace}.{className}{generics}";
+            if (!string.IsNullOrEmpty(nameSpace)) nameSpace += ".";
+            return $"{nameSpace}{className}{generics}";
         }
     }
 }
--- ContractClassWriter.cs
+++ ContractClassWriter.cs
@@ -6,21 +6,28 @@ namespace NetDoc
 {
     internal class ContractClassWriter
     {
-        public IEnumerable<string> ProcessCalls(string consumerName, IReadOnlyCollection<Call> calls)
+        public IEnumerable<string> ProcessCalls(string consumerName, IEnumerable<Call> calls)
         {
-            yield return $"private void UsedBy{consumerName}(";
-            var parameters = calls
+            var relevantCalls = calls
+                .Where(c => c.Method != "op_Equality")
+                .Where(c => c.Method != "op_Inequality")
+                .ToList();
+
+            yield return $"private void UsedBy{consumerName}<T0, T1>(";
+            var parameters = relevantCalls
                 .Where(c => !c.IsStatic)
                 .Where(c => !string.IsNullOrEmpty(c.Namespace))
-                .Select(c => $"        {c.TypeWithGenerics} {c.VariableName}")
+                .Select(c => c.TypeAsParameter)
+                .Concat(relevantCalls.SelectMany(c => c.ParameterTypes))
+                .Select(string.Intern)
                 .ToHashSet();
             Console.WriteLine(string.Join(",\r\n", parameters));
             Console.WriteLine("    )");
             Console.WriteLine("{");
-            List<int>.Enumerator foo;
-            foreach (var call in calls)
+
+            foreach (var invocation in relevantCalls.Select(c => c.Invocation).ToHashSet())
             {
-                Console.WriteLine($"    {call.VariableName}{call.Invocation};");
+                Console.WriteLine(invocation);
             }
 
             Console.WriteLine("}");
