7d40933868b32570b7f9ce8388d8e5677dc53969
Sam Blackburn <sam.blackburn@red-gate.com>
2020-06-05 16:34:56 +0100

    Woo! Unit tests!

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -9,10 +9,106 @@ namespace NetDoc.Tests
     class Tests
     {
         [Test]
-        public void MethodReference()
+        public void Constructor()
+        {
+            var referenced = Class("public ReferencedClass(int i) {}", "ReferencedClass");
+            var referencing = Class("public void Bar() { var _ = new ReferencedClass(3); }", "ReferencingClass");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void Field()
+        {
+            var referenced = Class("public int Foo;", "ReferencedClass");
+            var referencing = Class("public int Bar(ReferencedClass x) {return x.Foo;}", "ReferencingClass");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void Method()
         {
             var referenced = Class("public int Foo() => 3;", "ReferencedClass");
             var referencing = Class("public int Bar(ReferencedClass x) {return x.Foo();}", "ReferencingClass");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void VoidMethod()
+        {
+            var referenced = Class("public void Foo() {}", "ReferencedClass");
+            var referencing = Class("public void Bar(ReferencedClass x) {x.Foo();}", "ReferencingClass");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void Getter()
+        {
+            var referenced = Class("public int Foo => 3;", "ReferencedClass");
+            var referencing = Class("public int Bar(ReferencedClass x) {return x.Foo;}", "ReferencingClass");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void Setter()
+        {
+            var referenced = Class("public int Foo { get; set; }", "ReferencedClass");
+            var referencing = Class("public void Bar(ReferencedClass x) {x.Foo = 3;}", "ReferencingClass");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void IndexerGetter()
+        {
+            var referenced = Class("public bool this[int x] { get {return true;} set {} }", "ReferencedClass");
+            var referencing = Class("public bool Bar(ReferencedClass x) {return x[3];}", "ReferencingClass");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void IndexerSetter()
+        {
+            var referenced = Class("public bool this[int x] { get {return true;} set {} }", "ReferencedClass");
+            var referencing = Class("public void Bar(ReferencedClass x) {x[3] = false;}", "ReferencingClass");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void StaticField()
+        {
+            var referenced = Class("public static int Foo;", "ReferencedClass");
+            var referencing = Class("public static int Bar() {return ReferencedClass.Foo;}", "ReferencingClass");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void StaticMethod()
+        {
+            var referenced = Class("public static int Foo() => 3;", "ReferencedClass");
+            var referencing = Class("public static int Bar() {return ReferencedClass.Foo();}", "ReferencingClass");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void StaticVoidMethod()
+        {
+            var referenced = Class("public static void Foo() {}", "ReferencedClass");
+            var referencing = Class("public static void Bar() {ReferencedClass.Foo();}", "ReferencingClass");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void StaticGetter()
+        {
+            var referenced = Class("public static int Foo => 3;", "ReferencedClass");
+            var referencing = Class("public static int Bar() {return ReferencedClass.Foo;}", "ReferencingClass");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void StaticSetter()
+        {
+            var referenced = Class("public static int Foo { get; set; }", "ReferencedClass");
+            var referencing = Class("public static void Bar() {ReferencedClass.Foo = 3;}", "ReferencingClass");
             ContractAssertionShouldCompile(referencing, referenced);
         }
 
