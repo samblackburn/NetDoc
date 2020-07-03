0f875a35ac3726307a15fca3e069745f636f2c77
Sam Blackburn <sam.blackburn@red-gate.com>
2020-07-03 19:04:16 +0100

    Clarify this test class

--- Tests/AbstractTests.cs
+++ Tests/AbstractTests.cs
@@ -2,14 +2,28 @@
 
 namespace NetDoc.Tests
 {
+    /// <summary>
+    /// Ideally we'd produce assertions for inheritance hierarchies,
+    /// but it doesn't fit the one-liner style of contract assertions.
+    /// For now, we'll just ignore these since we don't often see
+    /// inheritance across API boundaries.
+    /// </summary>
     [TestFixture]
     class AbstractTests : TestMethods
     {
         [Test]
-        public void ObjectFactoryMethod()
+        public void CallToBaseConstructorIsIgnored()
         {
             var referenced = "public abstract class ReferencedClass {}";
             var referencing = "public class Derived : ReferencedClass {}";
+            ContractAssertionShouldBeEmpty(referencing, referenced);
+        }
+
+        [Test]
+        public void CallToBaseMethodIsIgnored()
+        {
+            var referenced = "public abstract class ReferencedClass { protected void Foo() {} }";
+            var referencing = "public class Derived : ReferencedClass { void Bar() => Foo(); }";
             ContractAssertionShouldBeEmpty(referencing, referenced);
         }
     }
