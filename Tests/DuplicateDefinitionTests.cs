6587667ad7dd6701e17b6c5f4cf4d512337b6a4f
Sam Blackburn <sam.blackburn@red-gate.com>
2020-09-18 17:34:57 +0100

    Red: Failing test to reproduce the dependency of:
    Microsoft.SqlServer.TransactSql.ScriptDom.dll has an IToken interface
    The AST parser has an interface with the same name and methods
    NetDoc nullrefs:
    
    --referencedFile SQLCompareEngine\\UI\\SqlServerGUI\\Schema\\SchemaGUI\\bin\\Debug\\net472\\RedGate.SQLCompare.ASTParser.dll
    --referencingDir SQLPrompt\\Build\\SsmsPackage18\\Debug
    --outDir .

---
+++ Tests/DuplicateDefinitionTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Tests.TestFramework;
+
+namespace Tests
+{
+    class InterfaceTests : TestMethods
+    {
+        [Test]
+        public void InterfaceDuplicatedBetweenDlls()
+        {
+            var inBoth = @"public interface IFoo {string Method();}";
+            var referencing = Class("public string Consumer(IFoo foo) => foo.Method();");
+            ContractAssertionShouldBeEmpty(referencing + inBoth, inBoth);
+        }
+    }
+}
