d7cd9107d396f33c436d57db06916bbb4a0fcefd
Sam Blackburn <sam.blackburn@red-gate.com>
2020-09-25 13:56:48 +0100

    Mscorlib test passes, Bar test fails with NotImplementedException

---
+++ Tests/ParametersTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Tests.TestFramework;
+
+namespace Tests
+{
+    class ParametersTests : TestMethods
+    {
+        [Test]
+        public void ParameterTypeInReferencedAssembly()
+        {
+            var referenced = Class(@"public static void Foo(Bar b) {}", "ReferencedClass")
+                             + Class("", "Bar");
+            var referencing = Class(@"public void Foo() => ReferencedClass.Foo(new Bar());");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void ParameterTypeInMscorlib()
+        {
+            var referenced = Class(@"public static void Foo(int b) {}", "ReferencedClass");
+            var referencing = Class(@"public void Foo() => ReferencedClass.Foo(3);");
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+    }
+}
