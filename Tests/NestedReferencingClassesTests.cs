24d589200bfc15a37c91eb3233ed215840f921dc
Sam Blackburn <sam.blackburn@red-gate.com>
2020-10-05 20:21:37 +0100

    Red tests for:
     - iterator state machine (implemented as a nested class in IL)
     - simple nested class example

---
+++ Tests/NestedReferencingClassesTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Tests.TestFramework;
+
+namespace Tests
+{
+    class NestedReferencingClassesTests : TestMethods
+    {
+        [Test]
+        public void ConsumerIsYieldReturnStateMachine()
+        {
+            var referenced = Class("public static int Foo() => 1;", "ReferencedClass");
+            var referencing = Class(@"System.Collections.Generic.IEnumerable<int> Bar() { yield return 1; yield return ReferencedClass.Foo(); }");
+
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+
+        [Test]
+        public void ConsumerHasNestedClass()
+        {
+            var referenced = Class("public static int Foo() => 1;", "ReferencedClass");
+            var referencing = Class(Class(@"int Bar() => ReferencedClass.Foo();", "NestedClass", null), "OuterClass");
+
+            ContractAssertionShouldCompile(referencing, referenced);
+        }
+    }
+}
