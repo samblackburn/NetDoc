bd459d6995c01e09e695d7eb29001f7b5ce87b50
Sam Blackburn <sam.blackburn@red-gate.com>
2025-10-06 16:59:16 +0100

    Red: Basic test for a delegate

---
+++ Tests/DelegatesTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Tests.TestFramework;
+
+namespace Tests;
+
+[TestFixture]
+internal class DelegatesTests : TestMethods
+{
+    [Test]
+    public void Delegate()
+    {
+        var referenced = "public delegate bool MyDelegate(int i);";
+        var referencing = Class("MyDelegate MakeDelegate() => i => i > 3;", "ReferencingClass");
+        ContractAssertionShouldCompile(referencing, referenced);
+    }
+}
