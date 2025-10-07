355e1d4b1d5d08bfb8c67bb9f4434ff85931160c
Sam Blackburn <sam.blackburn@red-gate.com>
2025-10-07 10:55:59 +0100

    Add ignored tests for cases we don't know how to fix

--- Tests/DelegatesTests.cs
+++ Tests/DelegatesTests.cs
@@ -7,10 +7,26 @@ namespace Tests;
 internal class DelegatesTests : TestMethods
 {
     [Test]
-    public void Delegate()
+    public void DelegateWithOneArgument()
     {
         var referenced = "public delegate bool MyDelegate(int i);";
         var referencing = Class("MyDelegate MakeDelegate() => i => i > 3;", "ReferencingClass");
+        ContractAssertionShouldCompile(referencing, referenced);
+    }
+
+    [Test, Ignore("Not sure how to infer the delegate's signature")]
+    public void DelegateWithTwoArguments()
+    {
+        var referenced = "public delegate bool MyDelegate(int i, int j);";
+        var referencing = Class("MyDelegate MakeDelegate() => (i, j) => i > j;", "ReferencingClass");
+        ContractAssertionShouldCompile(referencing, referenced);
+    }
+
+    [Test, Ignore("Not sure how to infer the delegate's signature")]
+    public void VoidDelegate()
+    {
+        var referenced = "public delegate void MyDelegate(int i);";
+        var referencing = Class("MyDelegate MakeDelegate() => i => {};", "ReferencingClass");
         ContractAssertionShouldCompile(referencing, referenced);
     }
 }
